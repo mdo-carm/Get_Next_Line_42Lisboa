b5faadd7a436c17c43779f0a1031e8d5ffa1626b
Miguel Do carmo tavares da cunha de abreu peixoto <mdo-carm@c1r18s7.42lisboa.com>
2022-05-03 19:06:19 +0100

    too many errors

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mdo-carm <mdo-carm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 17:28:50 by mdo-carm          #+#    #+#             */
-/*   Updated: 2022/04/26 21:58:59 by mdo-carm         ###   ########.fr       */
+/*   Updated: 2022/05/03 19:01:09 by mdo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,45 +17,66 @@ char	*final_output(char **output)
 	char	*final_output;
 	char	*temp;
 
-	if (!*output || !**output != '\0')
+	if (!*output || **output == '\0')
 		return (NULL);
-	temp = ft_strdup(*output);
 	if (ft_strchr(*output, '\n') != 0)
 	{
+		temp = ft_strdup(*output);
 		temp = ft_strchr(temp, '\n');
 		temp++;
 		final_output = ft_substr(*output, 0, ft_strlen(*output) - ft_strlen(temp));
 		free(*output);
-		*output = ft_strdup(temp);
-		printf("|%s|", final_output);
+		if (ft_strlen(temp) > 0)
+			*output = ft_strdup(temp);
+		printf("|%s|", final_output); //
 		return (final_output);
 	}
 	final_output = ft_substr(*output, 0, ft_strlen(*output));
-	printf("|%s|", final_output);
+	printf("|%s|", final_output); //
 	free(*output);
+	*output = NULL;
 	return (final_output);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*output[FOPEN_MAX];
+	static char	*output[OPEN_MAX];
 	char		*temp_buf;
+	char		*temp;
 	int			read_output;
 
-	if (BUFFER_SIZE <= 0 || fd < 0 || fd > FOPEN_MAX)
+	if (BUFFER_SIZE <= 0 || fd < 0 || fd > OPEN_MAX)
 		return (NULL);
 	temp_buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char *));
 	read_output = read(fd, temp_buf, BUFFER_SIZE);
-	if (!output[fd])
-		output[fd] = ft_strdup("");
+	if (output[fd] == NULL)
+		output[fd] = "\0";
 	while (read_output > 0)
 	{
 		temp_buf[read_output] = '\0';
 		output[fd] = ft_strjoin(output[fd], temp_buf);
-		if (ft_strchr(output[fd], '\n') != 0)
+		if (ft_strchr(temp_buf, '\n'))
 			break ;
 		read_output = read(fd, temp_buf, BUFFER_SIZE);
 	}
 	free(temp_buf);
-	return (final_output(&output[fd]));
+	temp_buf = NULL;
+	temp_buf = final_output(&output[fd]);
+	temp = temp_buf;
+	free(temp_buf);
+	return (temp);
+}
+
+int	main()
+{
+	int	fd;
+
+	fd = open("43_no_nl", O_RDONLY);
+	get_next_line(fd);
+	get_next_line(fd);
+	get_next_line(fd);
+	get_next_line(fd);
+	get_next_line(fd);
+	get_next_line(fd);
+	get_next_line(fd);
 }
